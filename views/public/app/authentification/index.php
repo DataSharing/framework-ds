3e24e725f293df22518c4c4d26fe525131d8ebe3
DataSharing <datasharing7@gmail.com>
2016-10-19 12:22:28 +0200

    update logo auth

--- views/public/app/authentification/index.php
+++ views/public/app/authentification/index.php
@@ -1,6 +1,6 @@
 <?php
 echo '<form class="form-signin" action="" method="post">';
-    //echo '<img class="logo-login" src="'.$this->base_url.'template/bootstrap/images/logo-login.png">';
+    	echo '<img class="logo-login" src="'.$this->base_url.'template/bootstrap/images/logo.png" width="350">';
         echo '<input type="email" autofocus="" required="" placeholder="Adresse mail" class="form-control" name="inputEmail"><br>';
         echo '<input type="password" required="" placeholder="Mot de passe" class="form-control" name="inputPassword"><br>';
     echo '<center><button type="submit" class="btn btn-primary">Connexion</button></center>';
